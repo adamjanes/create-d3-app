31014d2fe7e0c640891bc4494d308c41e946fde2
Adam Janes <amjanesy@gmail.com>
2019-08-06 16:28:06 +0200

    Initialize repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ChartWrapper from './ChartWrapper';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar bg="light">
+          <Navbar.Brand>CreateD3App</Navbar.Brand>
+        </Navbar>
+        <Container>
+          <Row>
+            <Col md={6} xs={12}><ChartWrapper /></Col>
+            <Col md={6} xs={12}></Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
